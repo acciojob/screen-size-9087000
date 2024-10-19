738694f3289c3da9608f9d7eb311dd1e44c8715a
9087000 <50361103+9087000@users.noreply.github.com>
2024-10-19 12:47:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 //your JS code here. If required.
-function updateSize() {
-            const width = window.innerWidth;
-            const height = window.innerHeight;
-
-            // Update the content of the spans in the h1 tag
-            document.getElementById('width').textContent = width;
-            document.getElementById('height').textContent = height;
-        }
-
-        // Initial update when the page loads
-        updateSize();
-
-        // Add event listener for window resize
-        window.addEventListener('resize', updateSize);
+ let height=this.innerHeight;
+    let width=this.innerWidth;
+   let div=document.getElementById("sizeInfo");
+    let h=div.firstChild;
+    h.innerText="Width: "+width+" and Height: "+height;  
+window.addEventListener("resize",function(){
+    let height=this.innerHeight;
+    let width=this.innerWidth;
+   let div=document.getElementById("sizeInfo");
+    let h=div.firstChild;
+ 
+    h.innerText="Width: "+width+" and Height: "+height; 
+  
+  })
