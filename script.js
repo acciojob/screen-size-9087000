b5249df03f71c3d5f2e2b43a93b2c630f0e893a2
9087000 <50361103+9087000@users.noreply.github.com>
2024-10-19 11:28:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-const app = require("./index.js");
-app.listen(3000, () => {
-  console.log('server started');
-});
+//your JS code here. If required.
+function updateSize() {
+            const width = window.innerWidth;
+            const height = window.innerHeight;
+
+            // Update the content of the spans in the h1 tag
+            document.getElementById('width').textContent = width;
+            document.getElementById('height').textContent = height;
+        }
+
+        // Initial update when the page loads
+        updateSize();
+
+        // Add event listener for window resize
+        window.addEventListener('resize', updateSize);
